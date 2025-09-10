46dbecc2bc74c8c3bb11dc2143df62bf21d532ee
thalles-cg <thalles.pity@gmail.com>
2025-09-10 10:07:55 -0300

    feat: configs iniciais

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-   res.send('Bem-vindo')
-})
-
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
