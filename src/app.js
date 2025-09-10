a96d68a431b7a9d9b875ee3d55883cf4cea23489
thalles-cg <thalles.pity@gmail.com>
2025-09-10 10:21:51 -0300

    feat: adição das rotas get e post para /usuarios

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from 'express';
 
 const app = express();
-
 app.use(express.json());
+
+const users = [];
+
+app.post('/usuarios', (req, res) => {
+   res.send('Ok')
+})
+
+app.get('/usuarios', (req, res) => {
+   res.send('Ok')
+})
 
 app.get('/', (req, res) => {
   res.send('Bem-vindo');
